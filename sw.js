fd6e69a6e7a8105dbed922cb3b6bb302399aa6c3
anyonsci <40672737+anyonsci@users.noreply.github.com>
2026-07-16 05:28:08 +0000

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-logger-cache-v1'
+const CACHE_NAME = 'attendance-logger-cache-v2'
 const APP_SHELL = [
   '/attendance_logger/',
   '/attendance_logger/index.html',
@@ -8,20 +8,42 @@ const APP_SHELL = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()),
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting()),
   )
 })
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
-    ).then(() => self.clients.claim()),
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
   )
 })
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') {
+    return
+  }
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((networkResponse) => {
+          const responseClone = networkResponse.clone()
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone)
+          })
+
+          return networkResponse
+        })
+        .catch(() => caches.match('/attendance_logger/index.html')),
+    )
+
     return
   }
 
@@ -33,10 +55,10 @@ self.addEventListener('fetch', (event) => {
 
       return fetch(event.request)
         .then((networkResponse) => {
-          const clonedResponse = networkResponse.clone()
+          const responseClone = networkResponse.clone()
 
           caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, clonedResponse)
+            cache.put(event.request, responseClone)
           })
 
           return networkResponse
